b4b13e587db3d55ddac4b641bd9c01b7b1d1b3f7
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-05-25 13:13:07 +0800

    perf: 抽离jwt鉴权逻辑并封装为中间件

--- app/router.js
+++ app/router.js
@@ -4,12 +4,12 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, middleware } = app;
+  const _jwt = middleware.jwtErr(app.config.jwt.secret);
   router.get('/', controller.home.index);
   router.post('/api/user/register', controller.user.register);
   router.post('/api/user/login', controller.user.login);
-  router.get('/api/user/test', controller.user.test);
-
+  router.get('/api/user/test', _jwt, controller.user.test);
 
   // router.get('/user/:id', controller.home.user);
   // router.get('/user', controller.home.user);
