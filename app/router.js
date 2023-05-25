d9a274f3dafbae159de726929e83bc0c3a91d600
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-05-25 16:37:06 +0800

    feat: 添加账单修改和账单编辑接口

--- app/router.js
+++ app/router.js
@@ -15,6 +15,8 @@ module.exports = app => {
   router.post('/api/upload', controller.upload.upload);
   router.post('/api/bill/add', _jwt, controller.bill.add); // 添加账单
   router.get('/api/bill/list', _jwt, controller.bill.list); // 获取账单列表
+  router.get('/api/bill/detail', _jwt, controller.bill.detail); // 获取详情
+  router.post('/api/bill/update', _jwt, controller.bill.update); // 账单更新
 
   // router.get('/user/:id', controller.home.user);
   // router.get('/user', controller.home.user);
